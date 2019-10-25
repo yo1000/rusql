77b7ec6245915ee1ab640ae301340bfad99af27c
yo1000 <yoichi.kikuchi.aug@gmail.com>
2019-10-25 16:30:38 +0900

    Add demo code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate mysql;
+extern crate r2d2_mysql;
+extern crate r2d2;
+
+use std::env;
+use std::sync::Arc;
+use std::thread;
+use mysql::{Opts,OptsBuilder};
+use r2d2_mysql::MysqlConnectionManager;
+
 fn main() {
-    println!("Hello, world!");
+	let db_url =  env::var("DATABASE_URL").unwrap();
+    let opts = Opts::from_url(&db_url).unwrap();
+    let builder = OptsBuilder::from_opts(opts);
+    let manager = MysqlConnectionManager::new(builder);
+    let pool = Arc::new(r2d2::Pool::builder().max_size(4).build(manager).unwrap());
+
+    let mut tasks = vec![];
+
+    for i in 0..3 {
+        let pool = pool.clone();
+        let th = thread::spawn(move || {
+            let mut conn = pool.get().unwrap();
+            conn.query("select user()").unwrap();
+            println!("thread {} end!" , i );
+        });
+        tasks.push(th);
+    }
+
+    for th in tasks {
+        let _ = th.join();
+    }
 }
